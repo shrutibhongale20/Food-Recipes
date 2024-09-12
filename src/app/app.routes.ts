691541f666c841bc482f4a89491e210bccab0b35
Shruti Bhongale <shrutibhongale20@gmail.com>
2024-09-13 00:05:45 +0530

    creating snacks list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './Pages/home/home.component';
 import { AboutComponent } from './Pages/about/about.component';
 import { RecipesComponent } from './Pages/recipes/recipes.component';
 import { SignInComponent } from './Pages/sign-in/sign-in.component';
+import { ListOfRecipesComponent } from './component/list-of-recipes/list-of-recipes.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent},
     {path:"about",component:AboutComponent},
     {path:"recipes",component:RecipesComponent},
-    {path:"sign-in",component:SignInComponent}
+    {path:"sign-in",component:SignInComponent},
+    {path:"list-of-recipes",component:ListOfRecipesComponent}
 ];
